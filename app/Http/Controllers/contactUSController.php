870769f0c69755e2df65d87462f2931f5b1eed6d
Antonio Pulido <rhema305@gmail.com>
2018-09-06 14:19:39 -0400

    sql query change for postgres

--- app/Http/Controllers/contactUSController.php
+++ app/Http/Controllers/contactUSController.php
@@ -14,7 +14,7 @@ return view('contactUS');
    /** * Show the application dashboard. * * @return \Illuminate\Http\Response */
    public function contactUSPost(Request $request)
    {
-    $this->validate($request, [ 'name' => 'required', 'email' => 'required|email', 'message' => 'required','phone' => 'optional', 'subject' => 'optional' ]);
+    $this->validate($request, [ 'name' => 'required', 'email' => 'required|email', 'message' => 'required','phone' => 'nullable|string', 'subject' => 'nullable|string' ]);
     ContactUS::create($request->all());
 
     return back()->with('success', 'Thanks for contacting us!');
