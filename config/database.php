0e9b2aa4464909d58621981dd1fddffc1739fd86
Antonio Pulido <rhema305@gmail.com>
2018-09-06 12:15:45 -0400

    Heroku fix

--- config/database.php
+++ config/database.php
@@ -1,14 +1,10 @@
 <?php
 
-    $host = $port = $username = $password = $database = '';
-    if(getenv("PGSQL_DATABASE_URL")){
-    $url = parse_url(getenv("PGSQL_DATABASE_URL"));
-    $host = $url["host"];
-    $post = $url["port"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
-}
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
     /*
